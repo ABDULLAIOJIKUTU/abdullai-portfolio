a7c378020be7726a68df20a2852d2d25688904f6
ojikutu abdullai olamide <ojikutuolamide06@gmail.com>
2025-07-28 09:15:10 +0100

    tailwindcss added

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
 const Navbar = () => {
-    return <div>
-      <nav></nav>
-  </div>;
+  return (
+    <div>
+      <nav>
+        <div>
+          <a href="/">OJIKUTU</a>
+        </div>
+        <ul>
+          <li>
+            <a href="/about">About</a>
+            <a href="/projects">Projects</a>
+            <a href="ref">reference</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
