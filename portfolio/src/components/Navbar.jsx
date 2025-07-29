d03afb9c8b7d56f22f8e203f38f77b54736fbebb
ojikutu abdullai olamide <ojikutuolamide06@gmail.com>
2025-07-29 21:21:58 +0100

    button component added

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -2,20 +2,30 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav className="bg-fuchsia-950">
-        <div>
-          <a href="/">OJIKUTU</a>
-        </div>
-        <ul>
-          <li>
-            <a href="/about">About</a>
-            <a href="/projects">Projects</a>
-            <a href="ref">reference</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    
+    <nav className="flex w-full flex-row items-center justify-between px-4 py-2 backdrop-blur-md">
+  <div className="flex items-center">
+    <a href="/" class="flex items-center">
+      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="#59168b" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-code-xml-icon lucide-code-xml">
+        <path d="m18 16 4-4-4-4" />
+        <path d="m6 8-4 4 4 4" />
+        <path d="m14.5 4-5 16" />
+      </svg>
+      <span className="ml-2 font-mono text-lg font-bold text-purple-900">Dev_ojikutu</span>
+    </a>
+  </div>
+
+  <div className="w-[70%] rounded-2xl px-6 py-2 font-mono font-bold backdrop-blur-lg">
+    <ul className="flex flex-row items-center gap-6 font-bold text-purple-800">
+      <li><a href="/" className="hover:border-b-2 hover:text-purple-600">Bio</a></li>
+      <li><a href="/" className="hover:border-b-2 hover:text-purple-600">Skills</a></li>
+      <li><a href="/" className="hover:border-b-2 hover:text-purple-600">Projects</a></li>
+      <li><a href="/" className="hover:border-b-2 hover:text-purple-600">Contact</a></li>
+    </ul>
+  </div>
+</nav>
+
+    
   );
 };
 
