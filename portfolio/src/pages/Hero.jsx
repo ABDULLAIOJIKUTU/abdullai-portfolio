79c88ece696ad72681baa4445b53531e7be3e735
ojikutu abdullai olamide <ojikutuolamide06@gmail.com>
2025-07-30 08:19:59 +0100

    "hero section"

---
+++ portfolio/src/pages/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "../../public/components/Button";
+
+const Hero = () => {
+  return (
+    <div>
+      <Button name={"Download CV"} />
+    </div>
+  );
+};
+
+export default Hero;
