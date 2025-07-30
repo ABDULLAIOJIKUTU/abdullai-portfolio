905a5038d2f4a535c6122724aa7b15b3795c22d3
ojikutu abdullai olamide <ojikutuolamide06@gmail.com>
2025-07-30 09:22:29 +0100

    add metadata and package files; include Hero component in App

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Navbar from "./components/Navbar.jsx";
 import Button from "./components/Button.jsx"; 
 
+
 const App = () => {
   return (
     <section className="w-full bg-[#c0bdbdc5] py-6 px-6 h-screen rounded-4xl">
       <Navbar />
       <Button />
+      <Hero />
     </section>
   );
 };
